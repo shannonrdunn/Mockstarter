a21651d15e0ec7866dd0200828ae738d46272f8b
Shannon Dunn <dunn.shannon@ge.com>
2015-06-01 17:43:56 -0700

    Got rid of user class, moved into Fund, more transactional that way...

--- lib/mockstarter.rb
+++ lib/mockstarter.rb
@@ -1,31 +1,49 @@
-require "mockstarter/version"
+#require "mockstarter/version"
 require "redis"
 require "json"
 
 module Mockstarter
 
-  class Data
-    def client
-      redis = Redis.new(:host => "192.168.59.103", :port => 6379)
-      return redis
+  class Fund
+    @@redis = Redis.new(:host => "192.168.59.103",
+                        :port => 6379)
+
+    def transaction(username, projectname, amount, creditcard)
+      id = Time.now.to_i.to_s
+      @@redis.hset('user:creditcard:' + username,
+                    creditcard)
+      @@redis.hset('user:transaction:' + username,
+                    projectname + ":" + id,
+                    amount)
+      @@redis.hset('project:transaction:' + projectname,
+                    username + ":" + id,
+                    amount)
     end
+
+    def card_check(creditcard)
+      ### TODO: create credit card check
+    end
+
+    def list_user(username)
+      puts @@redis.hgetall('user:transaction:' + username)
+    end
+
   end
 
-  class Users
-    def new_user
-      Data.client.hmset("hash", "f1", "v1", "f2", "v2")
-    end
-
-    def backed
-    end
-
-  end
-
-  class Payment
-
-  end
 
   class Project
+    @@redis = Redis.new(:host => "192.168.59.103",
+                        :port => 6379)
+
+    def create(projectname, goal)
+      @@redis.set('project:goal:' + projectname,
+                   goal)
+    end
+
+    def list(projectname)
+      puts @@redis.hgetall('project:transaction:' + projectname)
+      puts @@redis.get('project:goal:' + projectname)
+    end
 
   end
 end
