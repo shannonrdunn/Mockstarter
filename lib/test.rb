a82db4342020ead331b5f4249baf1f59d8e9e842
Shannon Dunn <dunn.shannon@ge.com>
2015-06-03 09:40:09 -0700

    delete random test file, useless

--- lib/test.rb
+++
@@ -1,5 +0,0 @@
-load 'mockstarter.rb'
-
-
-fund = Mockstarter::Fund.new(:username => "sdunn", :projectname => "kung fury", :amount => 823, :creditcard => 4342562230449520)
-project = Mockstarter::Project.new(:projectname => "kung fury", :goal => 50000)
